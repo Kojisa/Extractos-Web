cec32fb36f19473766c9645b4f75cbcbcb9bce07
Kojisa <federicoelias91@gmail.com>
2019-09-10 23:50:39 -0300

    Initial commit

---
+++ build/precache-manifest.baf3569b0f7d8fdb7f254f75bbf809c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "94cb8387fb6e833bdf0e",
+    "url": "/static/js/main.94cb8387.chunk.js"
+  },
+  {
+    "revision": "8cc6c9d72a8bfc918b73",
+    "url": "/static/js/1.8cc6c9d7.chunk.js"
+  },
+  {
+    "revision": "94cb8387fb6e833bdf0e",
+    "url": "/static/css/main.f97c3d09.chunk.css"
+  },
+  {
+    "revision": "01bae1443b7161907955a9ea2af88a2a",
+    "url": "/index.html"
+  }
+];
